a1c3f553363356d0814ba60c91dfcb8d79f14792
MattVogelsang <Vogelsangmatt@gmail.com>
2024-10-15 21:56:18 -0400

    Added a readme

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-# challenge-10-SVG-Logo-Maker
+# Challenge-10-SVG-Logo-Maker
+
+
+## Description
+
+A command-line application that allows users to create simple SVG logos by providing text, shape, and colors. Users can choose from a circle, triangle, or square to generate their logo.
+
+## Table of Contents 
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Features
+Enter up to three characters of text.
+Specify text color using color keywords or hexadecimal values.
+Choose from three shapes: circle, triangle, or square.
+Specify the shape's color using color keywords or hexadecimal values.
+Automatically generates an SVG file named logo.svg.
+Outputs a success message on the command line.
+
+## Usage
+
+
+
+SVG Logo Generator
+A command-line application that allows users to create simple SVG logos by providing text, shape, and colors. Users can choose from a circle, triangle, or square to generate their logo.
+
+
+bash
+Copy code
+npm install
+Usage
+Run the application using Node.js:
+
+bash
+Copy code
+node index.js
+Follow the prompts to enter your desired text, text color, shape color, and select a shape.
+
+Once you have completed the prompts, an SVG file named logo.svg will be created in the project directory. You can open this file in any web browser to view your generated logo.
+
+## Prompts
+When running the application, you will be prompted to provide the following information:
+
+Enter 3 or less characters text: (Input validation ensures that no more than three characters are entered.)
+Enter text color: (You can use standard color keywords like red, blue, or a hexadecimal value like #ff5733.)
+Enter shape color: (Similar to text color, specify using color keywords or hex values.)
+Choose shape: (Select from the following options: circle, triangle, square.)
+Shape Classes
+The application defines three shape classes, each responsible for generating their respective SVG representation:
+
+Circle: Generates an SVG circle with the specified text and colors.
+Triangle: Generates an SVG triangle with the specified text and colors.
+Square: Generates an SVG square with the specified text and colors.
+Each shape class can be found in the lib directory.
+
+License
+This project is licensed under the MIT License - see the LICENSE file for details.
--- index.js
+++ index.js
@@ -1,49 +1,48 @@
-const inquirer=require("inquirer")
-const Circle=require("./lib/circle")
-const Square=require("./lib/square")
-const Triangle=require("./lib/triangle")
-
-const fs=require("fs")
-
+const inquirer = require("inquirer");
+const Circle = require("./lib/circle");
+const Square = require("./lib/square");
+const Triangle = require("./lib/triangle");
+const fs = require("fs");
 
 inquirer.prompt([{
-    type:"input",
-    message:"Enter 3 or less characters text:",
-    validate:(text)=>text.length <=3 ? true:false,
-    name:"text"  
-},{
-    type:"input",
-    message:"Enter text color:",
-    name:"textColor"
+    type: "input",
+    message: "Enter 3 or less characters text:",
+    validate: (text) => text.length <= 3 ? true : "Text must be 3 characters or less",
+    name: "text"
+}, {
+    type: "input",
+    message: "Enter text color:",
+    name: "textColor"
+}, {
+    type: "input",
+    message: "Enter shape color:",
+    name: "shapeColor"
+}, {
+    type: "list",
+    message: "Choose shape:",
+    name: "shape",
+    choices: ["circle", "triangle", "square"]
+}])
+.then(res => {
+    let content = "";
+    let filename = "./logo.svg";
 
-},{
-    type:"input",
-    message:"Enter shape color:",
-    name:"shapeColor"
-
-},
-{
-    type:"list",
-    message:"choose shape:",
-    name:"shape",
-    choices:["circle","triangle","square"]
-}
-])
-.then(res=>{
-    let content=""
-    let filename=""
-    if(res.shape==="circle"){
-       const circle=new Circle(res.text,res.textColor,res.shapeColor)
-        content=circle.render()
-        filename="./examples/circle.svg"
-    }
-    else if(res.shape==="triangle"){
-
-    }
-    else if(res.shape==="square"){
-        
+    if (res.shape === "circle") {
+        const circle = new Circle(res.text, res.textColor, res.shapeColor);
+        content = circle.render();
+    } else if (res.shape === "triangle") {
+        const triangle = new Triangle(res.text, res.textColor, res.shapeColor);
+        content = triangle.render();
+    } else if (res.shape === "square") {
+        const square = new Square(res.text, res.textColor, res.shapeColor);
+        content = square.render();
     }
 
-    fs.writeFile(filename,content,(err)=>err ?console.log(err): console.log("success") )
-
-})
+    fs.writeFile(filename, content, (err) => {
+        if (err) {
+            console.log("Error generating logo:", err);
+        } else {
+            console.log("Generated logo.svg");
+        }
+    });
+});
